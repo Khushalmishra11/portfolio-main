d896970716f009a82aa91ec9808cf4e5759410cd
Khushal Mishra <129203300+Khushalmishra11@users.noreply.github.com>
2025-06-11 13:06:55 +0530

    Update ResumeButton.jsx

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ function ResumeButton() {
   return (
     <div className="fixed bottom-6 left-4 z-50">
       <a
-        href="../public/KhushalResume.pdf"
+        href="public/Khushal_Mishra.pdf"
         download
         className="bg-red-500 text-white text-sm sm:text-base px-4 sm:px-6 py-2 sm:py-3 rounded-full hover:bg-red-600 transition duration-300 shadow-lg hover:shadow-xl"
       >

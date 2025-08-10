c3ac101a6de90fbc7251f13493855bbb54b9f96a
Khushal Mishra <khushalmishra261203@gmail.com>
2025-08-10 21:30:27 +0530

    updated resume

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,11 +4,10 @@ function ResumeButton() {
   return (
     <div className="fixed bottom-6 left-4 z-50">
       <a
-        href="../src/assets/Khushal-Mishra.pdf"
-        download target="_blank"
+        href="https://drive.google.com/file/d/1bkoW9rKoRzUB3JBB-ArtJhxw0F5SLMLH/view?usp=sharing"
         className="bg-red-500 text-white text-sm sm:text-base px-4 sm:px-6 py-2 sm:py-3 rounded-full hover:bg-red-600 transition duration-300 shadow-lg hover:shadow-xl"
       >
-        Download my Resume
+        Resume
       </a>
     </div>
   );

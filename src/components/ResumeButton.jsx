aebb6ee8538452d48a454acc55b4a46e0eb3cd3d
Khushal Mishra <khushalmishra261203@gmail.com>
2025-08-10 21:32:01 +0530

    updated resume

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -5,6 +5,7 @@ function ResumeButton() {
     <div className="fixed bottom-6 left-4 z-50">
       <a
         href="https://drive.google.com/file/d/1bkoW9rKoRzUB3JBB-ArtJhxw0F5SLMLH/view?usp=sharing"
+        target="_blank"
         className="bg-red-500 text-white text-sm sm:text-base px-4 sm:px-6 py-2 sm:py-3 rounded-full hover:bg-red-600 transition duration-300 shadow-lg hover:shadow-xl"
       >
         Resume
